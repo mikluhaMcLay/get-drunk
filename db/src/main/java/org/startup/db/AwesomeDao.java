01cb371d49d8ae2af50f2ec26571c411ce6286a5
M. Borodin <borodin.mik@gmail.com>
2014-08-24 00:40:26 +0400

    хуй

--- db/src/main/java/org/startup/db/AwesomeDao.java
+++ db/src/main/java/org/startup/db/AwesomeDao.java
@@ -1,8 +1,29 @@
 package org.startup.db;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Component;
 import org.startup.entity.User;
 
+import javax.annotation.PostConstruct;
+
+@Component
 public class AwesomeDao {
+    private static final Logger log = LoggerFactory.getLogger( AwesomeDao.class );
+
+    @Autowired
+    private NamedParameterJdbcTemplate namedJdbcTemplate;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @PostConstruct
+    public void init() {
+        jdbcTemplate.execute( "SELECT 1" );
+        log.info( "All right" );
+    }
 
     public User getUser( long id ) {
         throw new UnsupportedOperationException();
@@ -11,7 +32,6 @@ public class AwesomeDao {
     public void saveUser( User user ) {
 
     }
-
 
 
 }
